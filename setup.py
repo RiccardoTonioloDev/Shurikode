2abb9953029acc1dbc96fe75f2ca2e5a2f7914c0
Riccardo <ssctonioloriccardo@gmail.com>
2025-02-01 11:50:04 +0100

    New version in the setup.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     sum="A 2D, highly redundant code.",
     author="Riccardo Toniolo",
     author_email="ssctonioloriccardo@gmail.com",
-    version="0.0.9",
+    version="0.1.0",
     license="MIT",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
